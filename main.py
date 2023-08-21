a6f708c1503ea69e0123c0dd184525756fcfc37b
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-08-22 05:55:48 +0700

    set up App class and inherited customtkinter.Ctk

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+
+
+class App(customtkinter.Ctk):
+    def __init__(self):
+        super().__init__()
+
+    # configure window
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
