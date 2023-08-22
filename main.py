53dd6a9e9d5d7b8ac4ed8dd8b31e43c76b63da5f
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-08-22 08:07:42 +0700

    created constants.py, set up app root geometry and title

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import tkinter as tk
 import customtkinter
+from constants import *
 
 customtkinter.set_appearance_mode("dark")
 
 
-class App(customtkinter.Ctk):
+class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+        self.geometry(f"{APP_WIDTH}x{APP_HEIGHT}")
+        self.title("Watch Your Dong")
 
     # configure window
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = App()
+    app.mainloop()
+    # print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
