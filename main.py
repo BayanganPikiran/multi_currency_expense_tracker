49da20d6bd59d49d233d36864ac59edaef637052
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-08-22 08:57:17 +0700

    created date_frame and date_pick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import customtkinter
 from constants import *
+from tkcalendar import Calendar, DateEntry
 
 customtkinter.set_appearance_mode("dark")
 
@@ -8,10 +9,23 @@ customtkinter.set_appearance_mode("dark")
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+
+        # configure window
         self.geometry(f"{APP_WIDTH}x{APP_HEIGHT}")
         self.title("Watch Your Dong")
 
-    # configure window
+        # configure grid layout
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure((1, 2, 3), weight=1)
+
+        # create date frame and widgets
+        self.date_frame = customtkinter.CTkFrame(self)
+        self.date_frame.pack(expand=True, fill=tk.BOTH)
+        self.date_pick = DateEntry(self.date_frame)
+        self.date_pick.grid(row=0, column=0)
+
+
+
 
 
 if __name__ == '__main__':
