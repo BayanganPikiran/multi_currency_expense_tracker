ad224b2b7a155a9699316b93f0066b89d17190da
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-08-26 05:25:23 +0700

    create Database class ande very starting structure thereof

---
+++ database.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+class Database:
+
+    def __init__(self, db_path):
+        self.expenses_db = db_path
+        self.conn = sqlite3.connect(self.expenses_db)
+        self.cursor = self.conn.cursor()
+        self.category_table = self.create_category_table()
+
+    def create_category_table(self):
+        pass
+
+
