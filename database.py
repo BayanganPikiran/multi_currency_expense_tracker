a20730fd81ca2312e90dfb288268a53e442d2394
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-09-01 05:26:34 +0700

    create_expense_table()

--- database.py
+++ database.py
@@ -16,3 +16,14 @@ class Database:
         self.conn.commit()
         return category_table
 
+    def create_expense_table(self):
+        expense_table = self.cursor.execute("""CREATE TABLE IF NOT EXISTS expense_record(
+            expense_id INTEGER(10) PRIMARY KEY AUTOINCREMENT, 
+            date TEXT(30) NUT NULL,
+            amount DECIMAL(10,2), 
+            category_fk 
+        """)
+        self.conn.commit()
+        return expense_table
+    
+
