6a9c99259ce4f112a580d53b7ce32e1196487360
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-09-01 09:21:14 +0700

    created record_expense()

--- database.py
+++ database.py
@@ -22,13 +22,18 @@ class Database:
             expense_id INTEGER(10) PRIMARY KEY AUTOINCREMENT, 
             date TEXT(30) NUT NULL,
             amount DECIMAL(10,2),
-            currency_used TEXT(30), 
+            currency TEXT(30), 
             category_fk INTEGER(10),
-            expense_description TEXT(200),
+            description TEXT(200),
             FOREIGN KEY(category_fk) REFERENCES Expense_category(category_id));             
         """)
         self.conn.commit()
         return expense_table
+
+    def record_expense(self, date, amt, curr, ctg, desc):
+        self.cursor.execute("INSERT INTO Expense_record(date, amount, currency, category_fk, description)"
+                            " VALUES(?, ?, ?, ?, ?)", (date, amt, curr, ctg, desc))
+        self.conn.commit()
 
 
 
