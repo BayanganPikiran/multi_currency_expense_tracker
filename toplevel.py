ee0214184046e7e905cabcf99218df1f78621385
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-09-17 10:40:11 +0700

    fixed StartingBalanceToplevel so it looks right

--- toplevel.py
+++ toplevel.py
@@ -52,6 +52,7 @@ class DepositTopLevel(ctk.CTkToplevel):
         self.geometry('225x85')
         self.title('Save Deposit')
         self.wm_transient()
+        self.configure(background="red")
         # parameters
         self.dep_date = date
         self.dep_curr = currency
@@ -73,33 +74,37 @@ class DepositTopLevel(ctk.CTkToplevel):
 
 
 class StartingBalanceToplevel(ctk.CTkToplevel):
-    def __init__(self, date, currency, amount):
+    def __init__(self):
         # window
         ctk.CTkToplevel.__init__(self)
-        self.geometry('225x100')
+        ctk.set_default_color_theme("green")
+        self.geometry("500x150")
         self.title("Starting Balance")
         self.wm_transient()
         self.balance_var = ctk.StringVar()
         # frames
-        self.date_frame = ctk.CTkFrame(self)
+        self.date_frame = ctk.CTkFrame(self, width=DATE_FRAME_WIDTH, height=DATE_FRAME_HEIGHT)
         self.date_frame.pack(expand=True, fill=ctk.BOTH)
         self.entry_frame = ctk.CTkFrame(self)
         self.entry_frame.pack(expand=True, fill=ctk.BOTH)
         self.button_frame = ctk.CTkFrame(self)
         self.button_frame.pack(expand=True, fill=ctk.BOTH)
         # date
-        self.start_date = DateEntry(self.date_frame, font=DATE_ENTRY_FONT)
-        self.start_date.pack(expand=True, fill=ctk.BOTH)
+        self.start_date = DateEntry(self.date_frame, font=DATE_ENTRY_FONT, width=10)
+        self.start_date.grid(row=0, column=1)
+        # self.start_date.pack(expand=True, fill=ctk.BOTH)
         # entry
         self.request_balance = ctk.CTkLabel(self.entry_frame, text="Please enter your starting balance",
-                                            font=LABEL_FONT)
-        self.request_balance.grid(row=0, column=0)
+                                            anchor='w', font=LABEL_FONT)
+        self.request_balance.grid(row=0, column=0, padx=10, sticky=ctk.NSEW)
         self.balance_entry = ctk.CTkEntry(self.entry_frame, textvariable=self.balance_var,
                                           width=ENTRY_WIDTH, height=ENTRY_HEIGHT)
         self.balance_entry.grid(row=1, column=0)
+        self.balance_curr_lbl = ctk.CTkLabel(self.entry_frame, text="Balance currency", anchor='w', font=LABEL_FONT)
+        self.balance_curr_lbl.grid(row=0, column=1)
         self.balance_curr = ctk.CTkComboBox(self.entry_frame, values=CURRENCIES,
                                             width=COMBOBOX_WIDTH, height=COMBOBOX_HEIGHT)
-        self.balance_curr.grid(row=1, column=2)
+        self.balance_curr.grid(row=1, column=1)
         # button
-        self.save_btn = ctk.CTkButton(self.button_frame, text="Save starting balance")
+        self.save_btn = ctk.CTkButton(self.button_frame, text="Save starting balance", font=BUTTON_FONT)
         self.save_btn.pack(expand=True, fill=ctk.BOTH)
